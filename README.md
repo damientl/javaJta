69805002ffbcc5304e69d4d009c772023eaa5d79
Damien Lutz <damien.lutz@operacao.rcadigital.com.br>
2019-09-10 13:37:54 -0300

    Creating initial microservices structure

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # javaJta
-Java JTA POC. Docker compose, Spring Boot, Spring Data Redis
+Java JTA POC. Docker compose, Spring Boot, Spring Data Redis, RabbitMQ, 
+
+#TODO
+Add spring-boot-starter-jta-atomikos 
---
+++ tenis-supplier-api/src/main/java/com/otherorg/tenis_store/TenisStoreApplication.java
@@ -0,0 +1,13 @@
+package com.otherorg.tenis_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenisStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenisStoreApplication.class, args);
+	}
+
+}
