334568dcf890080d6ee6b1bd7b79c94589aab86b
Damien Lutz <damien.lutz@operacao.rcadigital.com.br>
2019-10-22 20:48:15 -0300

    fixing connection factory

--- store-stock-api/src/main/java/com/myorg/store/stock/config/AppConfig.java
+++ store-stock-api/src/main/java/com/myorg/store/stock/config/AppConfig.java
@@ -3,19 +3,11 @@ package com.myorg.store.stock.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 @Configuration
 class AppConfig {
-
-  @Bean
-  public ReactiveRedisConnectionFactory connectionFactory() {
-    return new LettuceConnectionFactory("localhost", 6379);
-  }
 
 
   @Bean
